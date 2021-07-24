3862b557381f492642ad6d5f8a9dfe1187a7602b
harikrishna-gujje <harikrishnagujje1729@gmail.com>
2021-07-24 22:51:40 +0530

    added favicon icon for the blog website.

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from flaskblog import db
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
