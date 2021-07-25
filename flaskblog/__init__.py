6531a82744cfa1b916fe136ff7ef16ea651dabaf
harikrishna-gujje <harikrishnagujje1729@gmail.com>
2021-07-25 19:17:12 +0530

    added user integration to the database and validations are done to not store duplicate users.

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,12 @@
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 
 class RegistrationForm(FlaskForm):
@@ -9,6 +10,18 @@ class RegistrationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('ConfirmPassword', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Signup')
+
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username).first()
+        if user:
+            raise ValidationError('User already exits!')
+
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email).first()
+        if user:
+            raise ValidationError('Email already exits!')
 
 
 class LoginForm(FlaskForm):
